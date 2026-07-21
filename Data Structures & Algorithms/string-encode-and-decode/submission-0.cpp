39b70145d7b9b5b1565f4e44a829736fac6444ee
Abhinav Muley <muleyabhinav2811@gmail.com>
2026-07-22 01:43:10 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string encoded;
+        for(auto& i: strs){
+            encoded += to_string(i.size()) + "#" + i;
+        }
+        return encoded;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> result;
+        int i = 0;
+        while(i < s.size()){
+            int j = i;
+            while(s[j] != '#'){
+                j++;
+            }
+
+            int length = stoi(s.substr(i, j-i));
+            i = j + 1;
+            string word = s.substr(i, length);
+            result.push_back(word);
+            i = i + length;
+        }
+        return result;
+    }
+};
