043015fd328b15ceaa1b97095f6f7d4dc9d090f0
Abhinav Muley <muleyabhinav2811@gmail.com>
2026-07-25 22:54:55 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.cpp
@@ -0,0 +1,32 @@
+class MinStack {
+public:
+    stack<int> st;
+    stack<int> minst;
+
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        st.push(val);
+        if(!minst.empty()){
+            minst.push(min(val, minst.top()));
+        }
+        else{
+            minst.push(val);
+        }
+    }
+    
+    void pop() {
+        st.pop();
+        minst.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+       return minst.top();
+    }
+};
