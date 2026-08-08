4f63e12d0d99fda9ce694e9e35bcd48e7522b2bf
Abhinav Muley <muleyabhinav2811@gmail.com>
2026-08-09 02:17:50 +0530

    Add: island-perimeter - submission-6

---
+++ Data Structures & Algorithms/island-perimeter/submission-6.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();\
+        int dr[] = {0,0,1,-1};
+        int dc[] = {1,-1,0,0};
+        int perimeter = 0;
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(grid[i][j] == 1){
+                    perimeter += 4;
+                    for(int k = 0; k < 4; k++){
+                        int nr = i + dr[k];
+                        int nc = j + dc[k];
+
+                        if(nr >= 0 && nc >= 0 && nr < rows && nc < cols && grid[nr][nc] == 1){
+                            perimeter--;
+                        }
+                    }
+                }
+            }
+        }
+        return perimeter;
+    }
+};
