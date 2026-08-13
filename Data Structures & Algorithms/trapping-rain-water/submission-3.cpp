e0bf956481fabc9fb6bbb0f10fc1fe18b4804799
Abhinav Muley <muleyabhinav2811@gmail.com>
2026-08-13 12:52:30 +0530

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if (n <= 2) return 0;
+
+        int l = 0;
+        int r = n - 1;
+
+        int maxL = 0;
+        int maxR = 0;
+
+        int ans = 0;
+
+        while (l <= r) {
+
+            if (height[l] <= height[r]) {
+
+                // maxL is the relevant boundary
+                if (height[l] > maxL)
+                    maxL = height[l];
+                else
+                    ans += maxL - height[l];
+
+                l++;
+            }
+            else {
+
+                // maxR is the relevant boundary
+                if (height[r] > maxR)
+                    maxR = height[r];
+                else
+                    ans += maxR - height[r];
+
+                r--;
+            }
+        }
+
+        return ans;
+    }
+};
